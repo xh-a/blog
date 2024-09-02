3a3e11df3aa8c69b476b7d662ffce51ef5cf5f32
hui <2054043816@qq.com>
2024-09-02 16:26:00 +0800

    init push

---
+++ src/config.ts
@@ -0,0 +1,92 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+export const siteConfig: SiteConfig = {
+  title: 'hui blog',
+  subtitle: 'blog site',
+  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  themeColor: {
+    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,     // Hide the theme color picker for visitors
+  },
+  banner: {
+    enable: true,
+    src: 'assets/images/111024784_p0.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'center', // Equivalent to object-position, defaults center
+    credit: {
+      enable: true,         // Display the credit text of the banner image
+      text: 'for you',              // Credit text to be displayed
+      url: 'https://www.pixiv.net/artworks/121290061'                // (Optional) URL link to the original artwork or artist's page
+    }
+  },
+  favicon: [    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
+  ],
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+    {
+      name: 'GitHub',
+      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      external: true,                               // Show an external link icon and will open in a new tab
+    },
+  ],
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: 'assets/images/ac16947a0061895a.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Hui',
+  bio: 'just do it if you really want',
+  links: [
+    {
+      name: 'codeforce',
+      icon: 'simple-icons:codeforces',       // Visit https://icones.js.org/ for icon codes
+                                        // You will need to install the corresponding icon set if it's not already included
+                                        // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'https://codeforces.com/profile/jack-hui6',
+    },
+    {
+      name: 'Bilibili',
+      icon: 'fa6-brands:bilibili',       // Visit https://icones.js.org/ for icon codes
+                                        // You will need to install the corresponding icon set if it's not already included
+                                        // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'https://space.bilibili.com/631629722?spm_id_from=333.1007.0.0',
+    },
+    {
+      name: 'Csdn',
+      icon: 'tabler:circle-letter-c',       // Visit https://icones.js.org/ for icon codes
+      // You will need to install the corresponding icon set if it's not already included
+      // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'https://blog.csdn.net/weixin_51435884?spm=1000.2115.3001.5343',
+    },
+    {
+      name: 'Steam',
+      icon: 'fa6-brands:steam',
+      url: 'https://steamcommunity.com/profiles/76561199192607151/',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/xh-a',
+    },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
---
+++ src/content/posts/draft.md
@@ -0,0 +1,69 @@
+---
+title: Build and Deploy a Web Site with Github Action
+published: 2024-09-02
+tags: [Blogging, Demo, Website, Github]
+category: Website
+draft: true
+---
+
+In this guide, we will show you how to build and deploy a blog with Github Action.
+
+# Step 1: Create two repository, public and private
+
+For example, if we have a public repository named 'web_site' and a private repository named 'resource', we can write workflow in 'resource' to build and deploy the blog in 'web_site'.
+
+We need to create '.github/workflows/build.yml' in 'resource' repository, and the content is as follows:
+
+```yaml
+
+on:
+  push:
+    branches: [ "main" ]
+  workflow_dispatch:  # 手动触发
+
+permissions:
+  contents: read  
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+
+      - name: npm install, build, and test
+        run: |
+          npm install -g pnpm
+          pnpm install
+          pnpm build
+
+      - name: Deploy
+        uses: peaceiris/actions-gh-pages@v3
+        with:
+          deploy_key: ${{ secrets.DEPLOY_TOKEN }} # 另外还支持 github_token 和 personal_token
+          external_repository: xh-a/home # 修改为你的 GitHub Pages 仓库
+          publish_dir: ./dist
+          keep_files: false
+          publish_branch: main
+``` 
+The config use 'actions/checkout@v4' to pull the code, and use 'peaceiris/actions-gh-pages@v3' to deploy the code to 'web_site' repository.
+
+# Step 2: Generate a deploy token
+
+GitHub Pages action support three types of token: 'github_token', 'personal_token', 'deploy_key'.
+In this case, we use 'deploy_key' to deploy the code.
+First, we need to create public and private key pair, and we can use the following command to generate the key pair:
+
+```bash
+ssh-keygen -t rsa -b 4096 -C "$(git config user.email)" -f gh-pages -N ""
+# You will get 2 files:
+#   gh-pages.pub (public key)
+#   gh-pages     (private key)
+```
+
+Then, we need to add the public key to 'web_site' repository, and add the private key to 'resource' repository as a secret named 'DEPLOY_TOKEN'.
+
+We can set public key in the web page of 'web_site' repository by visiting 'Settings' -> 'Deploy keys' -> 'Add deploy key', and paste the content of 'gh-pages.pub' to the input box with random title and check 'Allow write access'.
+
+Then we can add the private key to 'resource' repository by visiting 'Settings' -> 'Secrets and variables' -> 'New repository secret', and paste the content of 'gh-pages' to the input box with the name 'DEPLOY_TOKEN'.
+
+Now, we can push the code to 'resource' repository, and the workflow will be triggered to build and deploy the code to 'web_site' repository.
